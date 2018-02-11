e16ea4f06441df5213150ab779ea6c27052c2294
snsmith92 <sherzelsmith@gmail.com>
2018-02-11 10:27:01 +0100

    Corrected game.rb to show belongs_to instead of belong to

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
-  belongs to :opponent, class_name: "User", foreign_key: "opponent_id"
+  belongs_to :opponent, class_name: "User", foreign_key: "opponent_id"
 end
