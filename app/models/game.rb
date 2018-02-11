a7a9caefe5f4cbf323af41107e18f104f1b8afef
snsmith92 <sherzelsmith@gmail.com>
2018-02-11 10:43:30 +0100

    Resolve conflicts in game.rb and user.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
-  
+  scope :available, -> { where(Game.arel_table[:white_player].not_eq(0)).where(black_player: nil) }
+
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
   belongs_to :opponent, class_name: "User", foreign_key: "opponent_id"
 end
