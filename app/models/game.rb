ae0359a6bb59ed42f02d1f1e8c4fb2e952fded08
snsmith92 <sherzelsmith@gmail.com>
2018-02-12 18:52:52 +0100

    Adjusted game model to remove black/white player and replace with owner/opponent

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
   belongs_to :opponent, class_name: "User", foreign_key: "opponent_id"
 
-  scope :available, -> { where(Game.arel_table[:white_player].not_eq(0)).where(black_player: nil) }
+  scope :available, -> { where(Game.arel_table[:owner].not_eq(0)).where(opponent: nil) }
 
 end
