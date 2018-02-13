9670adf4629487c2eb187b2c2e57321d5836fa17
snsmith92 <sherzelsmith@gmail.com>
2018-02-13 09:47:07 +0100

    Attempting to adjust db issues

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,6 @@
 class Game < ApplicationRecord
-  belongs_to :owner, class_name: "User", foreign_key: "owner_id"
-  belongs_to :opponent, class_name: "User", foreign_key: "opponent_id"
+  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  belongs_to :opponent, class_name: 'User', foreign_key: 'opponent_id'
 
   scope :available, -> { where(Game.arel_table[:owner].not_eq(0)).where(opponent: nil) }
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180205145440) do
-=======
 ActiveRecord::Schema.define(version: 20180130061540) do
 
->>>>>>> 0cc4e23403afdc19d21bffa64ba5a2a86c5afb4d
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,33 +20,6 @@ ActiveRecord::Schema.define(version: 20180130061540) do
     t.string   "name"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-<<<<<<< HEAD
-    t.integer  "black_player"
-    t.integer  "white_player"
-  end
-
-  create_table "pieces", force: :cascade do |t|
-    t.string   "type"
-    t.integer  "game_id"
-    t.string   "color"
-    t.string   "position_x"
-    t.string   "position_y"
-    t.boolean  "captured"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["game_id"], name: "index_pieces_on_game_id", using: :btree
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "username"
-    t.integer  "game_id"
-    t.boolean  "victory"
-    t.boolean  "turn"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["game_id"], name: "index_users_on_game_id", using: :btree
-=======
     t.integer  "white_player"
     t.integer  "black_player"
   end
@@ -70,7 +39,6 @@ ActiveRecord::Schema.define(version: 20180130061540) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
->>>>>>> 0cc4e23403afdc19d21bffa64ba5a2a86c5afb4d
   end
 
 end
