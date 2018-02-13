770d40ce49e0fdc343de8af1ed0c348af93e5a51
drewsef-916 <drewm541@gmail.com>
2018-02-13 18:53:02 +0000

    added piece subclass models

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,6 @@
 class Piece < ApplicationRecord
   belongs_to :game
+  delegate :king, :queen, :bishop, :knight, :rook, :pawn, to: :pieces
+
+  #needs models that inherit from piece called pawn, rook, knight, bishop, queen, king
 end
