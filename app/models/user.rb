f8325d5a012bf85f4f1ded6b4ff08f61957e682c
snsmith92 <sherzelsmith@gmail.com>
2018-02-13 10:23:57 +0100

    Fixed database issues, added username feature to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,39 @@
 class User < ApplicationRecord
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
 
+  # Db associations
   has_many :started_games, class_name: "Game", foreign_key: "owner_id"
   has_many :opponent_games, class_name: "Game", foreign_key: "opponent_id"
+
+  # Log in with username or email - associated models
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+        :recoverable, :rememberable, :trackable, 
+        :validatable, :authentication_keys => {email: true, login: false}
 end
--- db/migrate/20180205144904_create_users.rb
+++ db/migrate/20180205144904_create_users.rb
@@ -1,13 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
-      t.string :email
-      t.string :username
+    change_table :users do |t|
       t.integer :game_id
       t.boolean :victory
       t.boolean :turn
-
-      t.timestamps
     end
     add_index :users, :game_id
   end
