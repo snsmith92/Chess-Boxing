e9ed1bbc921f3dd74d3e93f1ba830c233e93d9a2
snsmith92 <sherzelsmith@gmail.com>
2018-02-20 17:12:32 +0100

    Syntax adjustments to populate_game! method

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,32 +7,32 @@ class Game < ApplicationRecord
 
   scope :available, -> { where(Game.arel_table[:owner].not_eq(0)).where(opponent = nil) }
   
-  def populate_game!(piece_params)
-    current_game = self.id
-    Piece.create("Rook", 0, 0, current_game, "white", false, "images/rook-white.png")
-    Piece.create("Knight", 1, 0, @game.id, "white", false, "images/knight-white.png")
-    Piece.create("Bishop", 2, 0, @game.id, "white", false, "images/bishop-white.png")
-    Piece.create("King", 3, 0, @game.id, "white", false, "images/king-white.png")
-    Piece.create("Queen", 4, 0, @game.id, "white", false, "images/queen-white.png")
-    Piece.create("Bishop", 5, 0, @game.id, "white", false, "images/bishop-white.png")
-    Piece.create("Knight", 6, 0, @game.id, "white", false, "images/knight-white.png")
-    Piece.create("Rook", 7, 0, @game.id, "white", false, "images/rook-white.png")
+  def populate_game!
+    # white pieces 
+    Piece.create(type: 'Rook', game_id: self.id, position_x: 0, position_y: 0, color: 'white', captured: false, image: '/images/rook-white.png')
+    Piece.create(type: 'Knight', game_id: self.id, position_x: 1, position_y: 0, color: 'white', captured: false, image: 'assets/images/knight-white.png')
+    Piece.create(type: 'Bishop', game_id: self.id, position_x: 2, position_y: 0, color: 'white', captured: false, image: 'assets/images/bishop-white.png')
+    Piece.create(type: 'King', game_id: self.id, position_x: 3, position_y: 0, color: 'white', captured: false, image: 'assets/images/king-white.png')
+    Piece.create(type: 'Queen', game_id: self.id, position_x: 4, position_y: 0, color: 'white', captured: false, image: 'assets/images/queen-white.png')
+    Piece.create(type: 'Bishop', game_id: self.id, position_x: 5, position_y: 0, color: 'white', captured: false, image: 'assets/images/bishop-white.png')
+    Piece.create(type: 'Knight', game_id: self.id, position_x: 6, position_y: 0, color: 'white', captured: false, image: 'assets/images/knight-white.png')
+    Piece.create(type: 'Rook', game_id: self.id, position_x: 7, position_y: 0, color: 'white', captured: false, image: 'assets/images/rook-white.png')
 
     (0..7).each do |n|
-      Piece.create("Pawn", n, 1, @game.id, "white", false, "images/pawn-white.png")
+      Piece.create(type: 'Pawn', game_id: self.id, position_x: n, position_y: 1, color: 'white', captured: false, image: 'images/pawn-white.png')
     end
 
-    Piece.create("Rook", 0, 7, @game.id, "black", false, "images/rook-black.png")
-    Piece.create("Knight", 1, 7, @game.id, "black", false, "images/knight-black.png")
-    Piece.create("Bishop", 2, 7, @game.id, "black", false, "images/bishop-black.png")
-    Piece.create("King", 3, 7, @game.id, "black", false, "images/king-black.png")
-    Piece.create("Queen", 4, 7, @game.id, "black", false, "images/queen-black.png")
-    Piece.create("Bishop", 5, 7, @game.id, "black", false, "images/bishop-black.png")
-    Piece.create("Knight", 6, 7, @game.id, "black", false, "images/knight-black.png")
-    Piece.create("Rook", 7, 7, @game.id, "black", false, "images/rook-black.png")
+    Piece.create(type: 'Rook', game_id: self.id, position_x: 0, position_y: 7, color:'black', captured: false, image: 'images/rook-black.png')
+    Piece.create(type: 'Knight', game_id: self.id, position_x: 1, position_y: 7, color: 'black', captured: false, image:'images/knight-black.png')
+    Piece.create(type: 'Bishop', game_id: self.id, position_x: 2, position_y: 7, color: 'black', captured: false, image: 'images/bishop-black.png')
+    Piece.create(type: 'King',game_id: self.id, position_x: 3, position_y: 7, color: 'black', captured: false, image: 'images/king-black.png')
+    Piece.create(type: 'Queen', game_id: self.id, position_x: 4, position_y: 7, color: 'black', captured: false, image: 'images/queen-black.png')
+    Piece.create(type: 'Bishop', game_id: self.id, position_x: 5, position_y: 7, color: 'black', captured: false, image: 'images/bishop-black.png')
+    Piece.create(type: 'Knight', game_id: self.id, position_x: 6, position_y: 7, color: 'black', captured: false, image: 'images/knight-black.png')
+    Piece.create(type: 'Rook', game_id: self.id, position_x: 7, position_y: 7, color: 'black', captured: false, image: 'images/rook-black.png')
 
     (0..7).each do |n|
-      Piece.create("Pawn", n, 6, @game.id, "black", false, "images/pawn-black.png")
+      Piece.create(type: 'Pawn', game_id: self.id, position_x: n, position_y: 6, color: 'black', captured: false, image: 'images/pawn-black.png')
     end
   end
 
