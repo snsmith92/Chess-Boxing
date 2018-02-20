8c74361dd55d6035e2cd567371be7d2b91c7723e
Iann Singer <itpsinger@gmail.com>
2018-02-20 16:38:56 +0000

    Fix syntax errors in game_controller_spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe GamesController, type: :controller do
   describe "games#update action" do
     it "should not allow unauthenticated users to join a game" do
       game = FactoryBot.create(:game, opponent: nil)
-      put :update, params: { id: game.id, game: { opponent: user.id }
+      put :update, params: { id: game.id, game: { opponent: user.id } }
       expect(response).to redirect_to new_user_session_path
     end
   end
@@ -28,7 +28,7 @@ RSpec.describe GamesController, type: :controller do
       sign_in user
       game = FactoryBot.create(:game, opponent: nil)
 
-      put :update, params: { id: game.id, game: { opponent: user.id }
+      put :update, params: { id: game.id, game: { opponent: user.id } }
       expect(response).to redirect_to game_path(game)
     end
   end
