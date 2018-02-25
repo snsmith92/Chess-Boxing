d1b72b9b0a78cf29297a7f715d05a81fde8db51c
drewsef-916 <drewm541@gmail.com>
2018-02-25 01:26:03 +0000

    added valid_move method for knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,5 +1,22 @@
 class Knight < Piece
   scope :piece, -> {where(type: 'Knight')}
 
+  def valid_move?(new_x, new_y)
+    return false if outside_board?(new_x, new_y)
+    #does not matter if knight is obstructed on its way to its new space; it can jump over other pieces
+    
+    #knight can move up 2, left or right 1
+    return true if self.position_x + 1 == new_x && self.position_y + 2 == new_y
+    return true if self.position_x - 1 == new_x && self.position_y + 2 == new_y
+    #knight can move down 2, left or right 1
+    return true if self.position_x + 1 == new_x && self.position_y - 2 == new_y
+    return true if self.position_x - 1 == new_x && self.position_y - 2 == new_y
+    #knight can move right 2, up or down 1
+    return true if self.position_x + 2 == new_x && self.position_y + 1 == new_y
+    return true if self.position_x + 2 == new_x && self.position_y - 1 == new_y
+    #knight can move left 2, up or down 1
+    return true if self.position_x - 2 == new_x && self.position_y + 1 == new_y
+    return true if self.position_x - 2 == new_x && self.position_y - 1 == new_y
+  end
 
-end
+end 
