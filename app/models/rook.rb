fd7895ee9734ee984b33c2f7379b9d2b2e19c141
snsmith92 <sherzelsmith@gmail.com>
2018-02-25 10:40:04 +0100

    Added valid_move? method for rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,4 +1,18 @@
 class Rook < Piece
   scope :piece, -> {where(type: 'Rook')}
 
+  def valid_move?(position_x, position_y)
+    self.position_x = current_x
+    self.position_y = current_y
+    position_x = x_destination
+    position_y = y_destination
+
+    if current_x == x_destination && is_obstructed_vertically? == false # vertical move
+      return true
+    elsif current_y == y_destination & is_obstructed_horizontally? == false # horizontal move
+      return true 
+    else 
+      return false
+    end 
+  end 
 end
