dd566cfcdbb56f3bdcdc05cd0bff22d08379fbf0
Iann Singer <itpsinger@gmail.com>
2018-02-27 12:12:58 +0000

    Add pawn move logic

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,6 +1,13 @@
 class Pawn < Piece
   scope :piece, -> {where(type: 'Pawn')}
 
+  move_count = 0
+  def count_moves
+    if #piece makes a move
+      move_count += 1
+    end
+  end
+
   def valid_move?(position_x, position_y)
     piece.position_x = x_current
     piece.position_y = y_current
@@ -10,4 +17,19 @@ class Pawn < Piece
     if outside_board?
       return false
 
+    elsif move_count == 0
+      if (y_destination - y_current).abs > 2
+        flash[:alert] = "This move is not allowed!"
+      return false
+    end
+
+  elsif move_count > 0
+    if (y_destination - y_current).abs > 1
+      flash[:alert] = "This move is not allowed!"
+      return false
+    end
+
+  elsif
+
+  end
 end
