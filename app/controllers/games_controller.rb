096742132d8fe8ed203397ba67da384935984e6c
LampshadeWarrior <32842513+LampshadeWarrior@users.noreply.github.com>
2018-03-01 17:01:18 -0500

    Fix tests (#31)
    * Fix existing tests
    
    * Fix is_obstructed_diagonally? and add tests for it

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,13 +10,16 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     @game.owner = current_user
-    @game.save!
+    @game.save
     redirect_to game_path(@game)
   end
 
   def show
     @game = Game.find(params[:id])
     @pieces = Piece.where(game_id: @game)
+    if user_signed_in? == false
+      redirect_to new_user_session_path
+    end
   end
 
   def update
@@ -26,7 +29,7 @@ class GamesController < ApplicationController
     # if @game.valid? && @game.owner != current_user.id
     if @game.owner != current_user.id
       @game.update_attribute(:opponent, current_user.id)
-      @game.save!
+      @game.save
       puts "SAVED"
       redirect_to game_path(@game)
     else
