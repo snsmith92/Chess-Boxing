76dcdb355eb9219697697f30b97fd73244b3d951
Iann Singer <itpsinger@gmail.com>
2018-03-08 03:34:09 +0000

    Remove comments

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,9 +1,6 @@
 class Queen < Piece
   scope :piece, -> {where(type: 'Queen')}
 
-  # class Queen < Piece
-  #   scope :piece, -> {where(type: 'Queen')}
-
     def valid_move?
       current_x = self.position_x
       current_y = self.position_y
@@ -26,25 +23,3 @@ class Queen < Piece
       end
     end
   end
-#
-#     self.position_x = current_x
-#     self.position_y = current_y
-#     position_x = x_destination
-#     position_y = y_destination
-#
-#     if current_x == x_destination # vertical move
-#       valid_move_vertical?(position_x, position_y)
-#     elsif current_y == y_destination # horizontal move
-#       valid_move_horizontal?(position_x, position_y)
-#     elsif (y_destination - y_current)/(x_destination - x_current) == 1 ||(y_destination - y_current)/(x_destination - x_current) == -1
-#       valid_move_diagnoal?(position_x,position_y)
-#     elsif outside_board?(position_x, position_y)
-#       return false
-#     elsif is_occupied?(piece_destination) && piece.user == current_user
-#       flash[:alert] = "You cannot capture your own piece!"
-#       return false
-#     else
-#       return true
-#     end
-#   end
-# end
