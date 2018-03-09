f6aa2521590b9aa73829e9cde11d70ab90e38730
drewsef-916 <drewm541@gmail.com>
2018-03-09 01:40:21 +0000

    final refactor

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -2,13 +2,11 @@ class Knight < Piece
   scope :piece, -> {where(type: 'Knight')}
 
   def valid_move?(new_x, new_y)
-    return false if outside_board?(new_x, new_y)
-    #does not matter if knight is obstructed on its way to its new space; it can jump over other pieces
+      return false if outside_board?(new_x, new_y)
 
-    if (self.position_x - new_x).abs + (self.position_y - new_y).abs == 3 &&
+      (self.position_x - new_x).abs + (self.position_y - new_y).abs == 3 &&
       (self.position_x - new_x).abs != 0 &&
       (self.position_y - new_y).abs != 0
-      return true
-    end 
+  end
 
 end 
---
+++ spec/models/knight_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+Rspec.describe Knight, type: :model do 
+  describe "valid_move?" do
+    it "should allow users to move 2 spaces in one direction, then 1 space in the other" do
+      
+    end
+  end
+end
