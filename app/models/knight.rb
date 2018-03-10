cfb550c9cb2a7b3229f45df3f64a83f153ddd754
drewsef-916 <30642898+drewsef-916@users.noreply.github.com>
2018-03-10 09:04:18 -0800

    Update knight.rb

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -2,10 +2,9 @@ class Knight < Piece
   scope :piece, -> {where(type: 'Knight')}
 
   def valid_move?(new_x, new_y)
-
-      (self.position_x - new_x).abs + (self.position_y - new_y).abs == 3 &&
-      (self.position_x - new_x).abs != 0 &&
-      (self.position_y - new_y).abs != 0
+    (self.position_x - new_x).abs + (self.position_y - new_y).abs == 3 &&
+    (self.position_x - new_x).abs != 0 &&
+    (self.position_y - new_y).abs != 0
   end
 
 end 
