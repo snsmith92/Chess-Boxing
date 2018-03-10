25ca4911bd87d1142031d7b9bf263d430454e45d
drewsef-916 <30642898+drewsef-916@users.noreply.github.com>
2018-03-10 09:00:32 -0800

    Update knight.rb

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -2,7 +2,6 @@ class Knight < Piece
   scope :piece, -> {where(type: 'Knight')}
 
   def valid_move?(new_x, new_y)
-      return false if outside_board?(new_x, new_y)
 
       (self.position_x - new_x).abs + (self.position_y - new_y).abs == 3 &&
       (self.position_x - new_x).abs != 0 &&
