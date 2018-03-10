a8731a5591f35c3de203297fddcec1a09a81bfd3
drewsef-916 <30642898+drewsef-916@users.noreply.github.com>
2018-03-10 09:02:48 -0800

    Update knight_spec.rb

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
-Rspec.describe Knight, type: :model do 
+RSpec.describe Knight, type: :model do 
   describe "valid_move?" do
-    it "should allow users to move 2 spaces in one direction, then 1 space in the other" do
-      
+    it "should return true for a move 2 spaces in one direction and 1 space in the other" do
+      user = FactoryBot.create(:user)
+      game = FactoryBot.create(:game, owner: user)
+      knight = FactoryBot.create(:knight, type: "Knight", game_id: game.id, moves: 0, position_x: 1, position_y: 2)
+      expect(knight.valid_move?(3, 3)).to eq(true)
     end
   end
+
+  describe "valid_move?" do
+    it "should return false for a move 3 spaces in one direction" do
+      user = FactoryBot.create(:user)
+      game = FactoryBot.create(:game, owner: user)
+      knight = FactoryBot.create(:knight, type: "Knight", game_id: game.id, moves: 0, position_x: 1, position_y: 2)
+      expect(knight.valid_move?(1, 5)).to eq(false)
+    end
+  end
+  
 end
