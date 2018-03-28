f2a168c2e82f43bac49e078e0ad885b2636e57f0
drewsef-916 <drewm541@gmail.com>
2018-03-28 22:49:21 +0000

    fixed a sprockets error

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -18,4 +18,27 @@ RSpec.describe Pawn, type: :model do
       expect(pawn.valid_move?(0, 4)).to eq(false)
     end
   end
+
+  describe "valid_move?" do
+    it "should return true if a pawn tries to capture diagonally" do
+      user = FactoryBot.create(:user)
+      opponent = FactoryBot.create(:user)
+      game = Game.create!(owner: user, opponent: opponent)
+      white_pawn = Pawn.create(user: user, game_id: game.id, color: "white", position_x: 2, position_y: 3)
+      black_knight = Knight.create(user: opponent, game_id: game.id, color: "black", position_x: 3, position_y: 4)
+      expect(white_pawn.valid_move?(3,4)).to eq(true)
+    end
+  end
+
+  describe "valid_move?" do
+    it "should return false if a pawn tries to capture to the front" do
+      user = FactoryBot.create(:user)
+      opponent = FactoryBot.create(:user)
+      game = Game.create!(owner: user, opponent: opponent)
+      white_pawn = Pawn.create(user: user, game_id: game.id, color: "white", position_x: 2, position_y: 3)
+      black_knight = Knight.create(user: opponent, game_id: game.id, color: "black", position_x: 3, position_y: 4)
+      black_rook = Rook.create(user: opponent, game_id: game.id, color: "black", position_x: 2, position_y: 4)
+      expect(white_pawn.valid_move?(2,4)).to eq(false)
+    end
+  end
 end
